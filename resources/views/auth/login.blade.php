d124f76272571740feedae28d27558491484d5f3
Ranipuspita <rani.puspita11@icloud.com>
2025-06-30 17:37:26 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>Login & Register</title>
+    @laravelPWA
 
     <style>
         :root {
